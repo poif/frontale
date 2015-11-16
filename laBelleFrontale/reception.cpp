6d74ab08870a5d1957a51a733c618c10420bf6bb
Florentp <florent@debianflo.home>
2015-11-16 14:48:00 +0100

    ajout des clés

--- laBelleFrontale/reception.cpp
+++ laBelleFrontale/reception.cpp
@@ -35,11 +35,13 @@ void reception::procReception(){
         QByteArray datagram;
         datagram.resize(soc->pendingDatagramSize());
         soc->readDatagram(datagram.data(),datagram.size());
+	
+	const unsigned char key[]={0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF}; 
 
         QString message;
 
         message = QString(datagram.data());
-
+	
 
         Message msg(message,'*');
         Requete req;
@@ -47,7 +49,7 @@ void reception::procReception(){
         string * showRep = new string[2];
         char * triq = new char(showRep[0].size()+showRep[1].size()+1);
 
-        //msg.dechiffrement();
+        msg.dechiffrement(key);
         req.decoupage(msg.getMsg().toStdString().c_str());
         req.construction();
         network_interface netinf;
@@ -65,13 +67,9 @@ void reception::procReception(){
 
         Message msg2(retour,'R', '*');
         msg2.entete();
-        //msg2.chiffrement();
+        msg2.chiffrement(key);
         client cli;
         cli.socBind();
         cli.emission(msg2.getChiffre());
-
-
-
-
 
 }
